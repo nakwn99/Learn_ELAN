5d0d3f94281a4eb2b9ec661cc2bebe4db1ca0d7c
nakwn99 <52886760+nakwn99@users.noreply.github.com>
2019-09-12 09:18:51 +0900

    %%% この関数の機能を説明してみる
    %%% 参考資料：　数理科学2019年6月号51ページ　「エネルギー地形解析」増田直紀先生による解説
    %%% 江崎先生による User's guide

--- mfunc_StateProb.m
+++ mfunc_StateProb.m
@@ -1,4 +1,17 @@
-function probMEM = mfunc_stateProb(h,J)
+%%% この関数の機能を説明してみる
+%%% 参考資料：　数理科学2019年6月号51ページ　「エネルギー地形解析」増田直紀先生による解説
+%%% 江崎先生による User's guidefunction probMEM = mfunc_stateProb(h,J)
+
+%%% この関数は、モデルのほうの確率 P_model(\sigma) を計算している　式 (5) は二つの項（単独、相互作用）がある二次モデルを示している　
+%%% パラメータ h, J は pfunc_02 で推定してあるものを使う
+%%% mfunc_VectorList で vectorList を作る　出来上がった vectorList は　nodeNum = 3 なら
+%%%   -1   1  -1   1  -1   1  -1   1
+%%%   -1  -1   1   1  -1  -1   1   1
+%%%   -1  -1  -1  -1   1   1   1   1
+%%% になる
+%%% Z は、式（4）の分母　分配関数と呼ばれる関数の計算になる　これで割り算することで確率になる
+%%% 
+
 
 nodeNumber = size(h,1);
 vectorList = mfunc_VectorList(nodeNumber);
@@ -7,6 +20,8 @@ numVec = size(vectorList,2);
 Z = sum(exp(-(-diag((0.5*J*vectorList)' * vectorList) - sum(((h * ones(1,numVec)) .* vectorList)', 2))),1);
 probMEM = exp(-(-diag((0.5*J*vectorList)' * vectorList) - sum(((h * ones(1,numVec)) .* vectorList)', 2))) ./ Z;
 
-
+%%% この関数の機能を説明してみる
+%%% 参考資料：　数理科学2019年6月号51ページ　「エネルギー地形解析」増田直紀先生による解説
+%%% 江崎先生による User's guide
 
 
