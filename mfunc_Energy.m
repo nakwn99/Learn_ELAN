ffe7961f0d842cf31e18ac1e05e44a27bf40b4e4
nakwn99 <52886760+nakwn99@users.noreply.github.com>
2019-09-14 18:36:33 +0900

    %%% モデルから推定した h, J を用いて、式（5）に基づいてエネルギーを計算する
    %%% 資料：数理科学2019年6月号51ページ「エネルギー地形解析」増田直紀先生による解説　江崎先生による User's guide

--- mfunc_Energy.m
+++ mfunc_Energy.m
@@ -1,3 +1,6 @@
+%%% モデルから推定した h, J を用いて、式（5）に基づいてエネルギーを計算する
+%%% 資料：数理科学2019年6月号51ページ「エネルギー地形解析」増田直紀先生による解説　江崎先生による User's guide
+
 function E = mfunc_Energy(h,J)
 % Calculate energy
 
@@ -8,6 +11,5 @@ numVec = size(vectorList,2);
 E = -diag((0.5*J*vectorList)' * vectorList) ...
     - sum(((h * ones(1,numVec)) .* vectorList)', 2);
 
-
-
-
+%%% モデルから推定した h, J を用いて、式（5）に基づいてエネルギーを計算する
+%%% 資料：数理科学2019年6月号51ページ「エネルギー地形解析」増田直紀先生による解説　江崎先生による User's guide
